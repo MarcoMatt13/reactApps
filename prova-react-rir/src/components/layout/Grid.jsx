e913d4b3c05ee8bcfee6e67c329bbba8cddfaf85
Marco Mattietto <marco.mattietto@h-farm.com>
2022-02-04 18:03:29 +0100

    small update

--- prova-react-rir/src/components/layout/Grid.jsx
+++ prova-react-rir/src/components/layout/Grid.jsx
@@ -11,7 +11,6 @@ const Grid = (props) => {
       title: "This is a first exp",
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
@@ -20,7 +19,6 @@ const Grid = (props) => {
       title: "This is a second exp",
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
@@ -29,7 +27,6 @@ const Grid = (props) => {
       title: "This is a third exp",
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
@@ -38,7 +35,6 @@ const Grid = (props) => {
       title: "This is a fourth exp",
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
@@ -47,7 +43,6 @@ const Grid = (props) => {
       title: "This is a fifth exp",
       image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-      address: "Meetupstreet 5, 12345 Meetup City",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
