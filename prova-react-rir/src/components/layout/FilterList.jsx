8e5b3c2a9e509b468989ce35b3ceb62670ecf6f0
Marco Mattietto <marco.mattietto@h-farm.com>
2022-02-07 12:56:47 +0100

    playing with filters

--- prova-react-rir/src/components/layout/FilterList.jsx
+++ prova-react-rir/src/components/layout/FilterList.jsx
@@ -5,19 +5,21 @@ import "antd/dist/antd.css";
 import classes from "./prova.module.css";
 import Grid from "./Grid";
 
-const FilterList = ({ filterExp }) => {
+const FilterList = (props) => {
   const state = {
     value: 1,
   };
 
   return (
-    <Radio.Group onChange={filterExp} style={{ padding: "20px" }}>
+    <Radio.Group onChange={props.filterExp} style={{ padding: "20px" }}>
       <Space direction="vertical">
-        <Radio value="1">Display id 1</Radio>
-        <Radio value="2">Display id 2</Radio>
-        <Radio value="3">Display id 3</Radio>
-        <Radio value="4">Display id 4</Radio>
-        <Radio value="5">Display id 5</Radio>
+        {props.contents.map((exp, index) => {
+          return (
+            <Radio value={index} key={index} name={exp}>
+              {exp}
+            </Radio>
+          );
+        })}
       </Space>
     </Radio.Group>
   );
--- prova-react-rir/src/components/layout/LayoutModel.jsx
+++ prova-react-rir/src/components/layout/LayoutModel.jsx
@@ -3,7 +3,7 @@ import Grid from "./Grid";
 import FilterList from "./FilterList";
 import classes from "./prova.module.css";
 import { Layout, Menu } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const { SubMenu } = Menu;
 
@@ -16,23 +16,26 @@ const LayoutModel = () => {
       title: "This is a first exp",
       image:
         "https://sognienumeri.it/wp-content/uploads/2019/02/Numero-1-300x300.png",
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      description: "Id1, Padova, Comune di Padova",
+      owners: "Comune di Padova",
+      locations: "Padova",
     },
     {
       id: "2",
       title: "This is a second exp",
       image: "https://media.leroymerlin.it/media/233565/.jpg",
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      description: "Id2, Comune di Venezia, Venezia",
+      owners: "Comune di Venezia",
+      locations: "Venezia",
     },
     {
       id: "3",
       title: "This is a third exp",
       image:
         "https://cdn.xxl.thumbs.canstockphoto.it/3-nero-pulito-3d-isolato-numero-bianco-archivio-fotografico_csp60215704.jpg",
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      description: "Id3, famiglia Panozzo, Dolomiti",
+      owners: "Famiglia Panozzo",
+      locations: "Dolomiti",
     },
     {
       id: "4",
@@ -41,6 +44,8 @@ const LayoutModel = () => {
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      owners: "Consorzio Agrario",
+      locations: "Venezia",
     },
     {
       id: "5",
@@ -49,18 +54,38 @@ const LayoutModel = () => {
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
       description:
         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      owners: "Comune di Venezia",
+      locations: "Venezia",
     },
   ];
 
-  const [experiences, setExperiences] = useState("all");
+  const locations = ["Padova", "Venezia", "Dolomiti", "Treviso"];
+
+  const owners = [
+    "Comune di Venezia",
+    "Famiglia Panozzo",
+    "Comune di Padova",
+    "Consorzio Agrario",
+  ];
+
+  const [locationslist, setLocationslist] = useState("all");
+  const [ownerslist, setOwnerslist] = useState("all");
 
   const filteredExp =
-    experiences === "all"
+    locationslist && ownerslist === "all"
       ? dummy
-      : dummy.filter((dummy) => dummy.id.includes(experiences));
+      : dummy
+          .filter((dummy) => dummy.locations.includes(locationslist))
+          .filter((dummy) => dummy.owners.includes(ownerslist));
 
-  const filterExp = (e) => {
-    setExperiences(e.target.value);
+  const filterLocations = (e) => {
+    setLocationslist(locations[e.target.value]);
+    console.log(locations[e.target.value]);
+  };
+
+  const filterOwners = (e) => {
+    setOwnerslist(owners[e.target.value]);
+    console.log(owners[e.target.value]);
   };
 
   return (
@@ -71,20 +96,11 @@ const LayoutModel = () => {
         <Layout>
           <Sider style={{ padding: "24px 0", background: "white" }}>
             <Menu mode="inline" style={{ height: "100%" }}>
-              <SubMenu key="sub1" title="subnav 1">
-                <FilterList filterExp={filterExp} />
+              <SubMenu key="sub1" title="Location">
+                <FilterList filterExp={filterLocations} contents={locations} />
               </SubMenu>
-              <SubMenu key="sub2" title="subnav 2">
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" title="subnav 3">
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
+              <SubMenu key="sub2" title="Owner">
+                <FilterList filterExp={filterOwners} contents={owners} />
               </SubMenu>
             </Menu>
           </Sider>
